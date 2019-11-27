cb42e135e3a65999c79f10a35862ecc17a1361de
Tomasz Walotek <tomasz.walotek@gmail.com>
2019-11-27 11:21:59 +0000

    setup: Add install script

---
+++ setup/install.py
@@ -0,0 +1,27 @@
+import os
+
+
+PACKAGES = [
+    # Sway
+    "community/sway",
+    "aur/swaylock-effects-git",
+    "community/rofi",
+
+    # Fonts
+    "community/otf-font-awesome",
+    "aur/ttf-input",
+
+    # Shell utils
+    "community/fd",
+    "community/fzf",
+    "extra/tree",
+]
+
+
+def install(packages):
+    pkgs = " ".join(packages)
+    os.system(f"yay -S --needed {pkgs}")
+
+
+if __name__ == "__main__":
+    install(PACKAGES)
