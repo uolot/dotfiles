1f39520ef1e90773d052c3ae670846b242a5a49c
Tomasz Walotek <tomasz.walotek@gmail.com>
2019-12-10 10:29:16 +0000

    setup: Add groups.py for adding user to groups

---
+++ setup/groups.py
@@ -0,0 +1,24 @@
+import os
+import getpass
+from lib import ask
+
+
+GROUPS = [
+    "docker",  # docker
+    "input",
+    "lp",  # bluetooth
+    "video",  # brightness
+]
+USERNAME = getpass.getuser()
+
+
+def add_user_to_groups():
+    if not ask("Add user to groups?"):
+        return
+
+    for group in GROUPS:
+        os.system(f"sudo gpasswd -a {USERNAME} {group}")
+
+
+if __name__ == "__main__":
+    add_user_to_groups()
