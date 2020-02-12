dfb6d71404c162c92dd2d7731cebfbdd5060c9a5
Tomasz Walotek <tomasz.walotek@gmail.com>
2020-02-12 11:42:59 +0000

    powermenu: Lock screen

--- bin/powermenu.sh
+++ bin/powermenu.sh
@@ -2,9 +2,16 @@
 
 option=$(echo -e "Suspend+Hibernate\nHibernate\nShutdown\nReboot" | rofi -dmenu)
 
+lock=true
+powercmd=true
+
 case "$option" in
-    "Suspend+Hibernate") systemctl suspend-then-hibernate ;;
-    "Hibernate") systemctl hibernate ;;
-    "Shutdown") shutdown -h now ;;
-    "Reboot") systemctl reboot ;;
+    "Suspend+Hibernate") powercmd="systemctl suspend-then-hibernate" ;;
+    "Hibernate") powercmd="systemctl hibernate" ;;
+    "Shutdown") powercmd="shutdown -h now" ;;
+    "Reboot") powercmd="systemctl reboot" ;;
+    *) lock=false ;;
 esac
+
+$($lock) && swaylock --daemonize --clock --indicator --screenshots --effect-blur 20x2 --effect-vignette 0.5:0.5
+$($powercmd)
