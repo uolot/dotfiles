1bd2315c8e69e09ad3d7703e1663d66e0fa7e5cf
Tomasz Walotek <tomasz.walotek@gmail.com>
2020-09-01 21:53:18 +0200

    sway: Fix rofi monitor by replacing plain rofi with rofi-sway script

--- bin/powermenu.sh
+++ bin/powermenu.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
-option=$(echo -e "suspend+hibernate\nhibernate\nshutdown\nreboot" | rofi -dmenu -monitor 0)
+option=$(echo -e "suspend+hibernate\nhibernate\nshutdown\nreboot" | $HOME/Bin/rofi-sway -dmenu -monitor 0)
 
 lock=true
 powercmd=true
