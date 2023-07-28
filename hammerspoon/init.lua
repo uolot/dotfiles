281a138febd0bb512e86af06067ebca97317af40
Tomasz Walotek <tomasz.walotek@gmail.com>
2023-07-28 20:20:42 +0200

    hammerspoon: Lint + update

--- hammerspoon/init.lua
+++ hammerspoon/init.lua
@@ -9,11 +9,11 @@ local itermID = "com.googlecode.iterm2"
 -- local outlookID = "com.microsoft.outlook"
 
 local mouseCircleConfig = {
-    radius=40,
-    color={red=1, blue=0, green=0, alpha=0.4},
-    fill=true,
-    strokeWidth=5,
-    duration=0.4,
+    radius = 40,
+    color = { red = 1, blue = 0, green = 0, alpha = 0.4 },
+    fill = true,
+    strokeWidth = 5,
+    duration = 0.4,
 }
 
 hs.window.animationDuration = 0
@@ -29,7 +29,7 @@ local function toggleTeamsMute()
     local teams = hs.application.find(teamsID)
     -- hs.application.findWindow(titlePattern)
     if not (teams == null) then
-        hs.eventtap.keyStroke({"cmd", "shift"}, "m", 50, teams)
+        hs.eventtap.keyStroke({ "cmd", "shift" }, "m", 50, teams)
     end
 end
 
@@ -37,7 +37,7 @@ local function startIterm()
     -- XXX: try hs.application.launchOrFocus(itermID)
     local iterm = hs.application.find(itermID)
     if not (iterm == null) then
-        hs.eventtap.keyStroke({"cmd"}, "n", 0, iterm)
+        hs.eventtap.keyStroke({ "cmd" }, "n", 0, iterm)
     end
 end
 
@@ -73,7 +73,7 @@ local function mouseHighlight()
     local mousepoint = hs.mouse.getAbsolutePosition()
     -- Prepare a big red circle around the mouse pointer
     local r = mouseCircleConfig.radius
-    mouseCircle = hs.drawing.circle(hs.geometry.rect(mousepoint.x-r, mousepoint.y-r, 2*r, 2*r))
+    mouseCircle = hs.drawing.circle(hs.geometry.rect(mousepoint.x - r, mousepoint.y - r, 2 * r, 2 * r))
     mouseCircle:setStrokeColor(mouseCircleConfig.color)
     mouseCircle:setFillColor(mouseCircleConfig.color)
     mouseCircle:setFill(mouseCircleConfig.fill)
@@ -81,11 +81,14 @@ local function mouseHighlight()
     mouseCircle:show()
 
     -- Set a timer to delete the circle after 0.4 second
-    mouseCircleTimer = hs.timer.doAfter(mouseCircleConfig.duration, function() mouseCircle:delete(); mouseCircle = nil end)
+    mouseCircleTimer = hs.timer.doAfter(mouseCircleConfig.duration,
+        function()
+            mouseCircle:delete(); mouseCircle = nil
+        end)
 end
 
 local function yabai(args)
-    hs.task.new("/usr/local/bin/yabai", nil, function (...)
+    hs.task.new("/usr/local/bin/yabai", nil, function(...)
         print("stream", hs.inspect(table.pack(...)))
         return true
     end, args):start()
@@ -98,8 +101,8 @@ end
 --     end, args):start()
 -- end
 --
-local hyper = {"cmd", "alt", "ctrl", "shift"}
-local meh = {"alt", "ctrl", "shift"}
+local hyper = { "cmd", "alt", "ctrl", "shift" }
+local meh = { "alt", "ctrl", "shift" }
 
 -- HYPER
 
@@ -114,19 +117,19 @@ hs.hotkey.bind(hyper, "i", startIterm)
 
 -- Window navigation
 -- hs.hotkey.bind(hyper, ";", hs.hints.windowHints)
-hs.hotkey.bind(hyper, "return", function () yabai({'-m', 'window', '--toggle', 'zoom-fullscreen'}) end)
-hs.hotkey.bind(hyper, "space", function () yabai({'-m', 'window', '--toggle', 'float'}) end)
+hs.hotkey.bind(hyper, "return", function() yabai({ '-m', 'window', '--toggle', 'zoom-fullscreen' }) end)
+hs.hotkey.bind(hyper, "space", function() yabai({ '-m', 'window', '--toggle', 'float' }) end)
 
-hs.hotkey.bind(hyper, "h", function () yabai({'-m', 'window', '--focus', 'west'}) end)
-hs.hotkey.bind(hyper, "j", function () yabai({'-m', 'window', '--focus', 'south'}) end)
-hs.hotkey.bind(hyper, "k", function () yabai({'-m', 'window', '--focus', 'north'}) end)
-hs.hotkey.bind(hyper, "l", function () yabai({'-m', 'window', '--focus', 'east'}) end)
+hs.hotkey.bind(hyper, "h", function() yabai({ '-m', 'window', '--focus', 'west' }) end)
+hs.hotkey.bind(hyper, "j", function() yabai({ '-m', 'window', '--focus', 'south' }) end)
+hs.hotkey.bind(hyper, "k", function() yabai({ '-m', 'window', '--focus', 'north' }) end)
+hs.hotkey.bind(hyper, "l", function() yabai({ '-m', 'window', '--focus', 'east' }) end)
 
-hs.hotkey.bind(hyper, "n", moveWindowToNextScreen)
-hs.hotkey.bind(hyper, "p", moveWindowToPreviousScreen)
+hs.hotkey.bind(hyper, "n", function() yabai({ '-m', 'window', '--space', 'next' }) end)
+hs.hotkey.bind(hyper, "p", function() yabai({ '-m', 'window', '--space', 'prev' }) end)
 
-hs.hotkey.bind(hyper, "right", function () yabai({'-m', 'display', '--focus', 'next'}) end)
-hs.hotkey.bind(hyper, "left", function () yabai({'-m', 'display', '--focus', 'prev'}) end)
+hs.hotkey.bind(hyper, "right", function() yabai({ '-m', 'display', '--focus', 'next' }) end)
+hs.hotkey.bind(hyper, "left", function() yabai({ '-m', 'display', '--focus', 'prev' }) end)
 
 -- hs.hotkey.bind(hyper, "z", function () warpd({'--hint'}) end)
 -- hs.hotkey.bind(hyper, "x", function () warpd({'--grid'}) end)
@@ -150,21 +153,27 @@ hs.hotkey.bind(hyper, "m", mouseHighlight)
 -- A . D F . G . . . ;
 -- Z . C V . . M x x /
 
-hs.hotkey.bind(meh, "h", function () yabai({'-m', 'window', '--swap', 'west'}) end)
-hs.hotkey.bind(meh, "j", function () yabai({'-m', 'window', '--swap', 'south'}) end)
-hs.hotkey.bind(meh, "k", function () yabai({'-m', 'window', '--swap', 'north'}) end)
-hs.hotkey.bind(meh, "l", function () yabai({'-m', 'window', '--swap', 'east'}) end)
+hs.hotkey.bind(meh, "h", function() yabai({ '-m', 'window', '--swap', 'west' }) end)
+hs.hotkey.bind(meh, "j", function() yabai({ '-m', 'window', '--swap', 'south' }) end)
+hs.hotkey.bind(meh, "k", function() yabai({ '-m', 'window', '--swap', 'north' }) end)
+hs.hotkey.bind(meh, "l", function() yabai({ '-m', 'window', '--swap', 'east' }) end)
 
-hs.hotkey.bind(meh, "space", function () yabai({'-m', 'window', '--toggle', 'float', '--grid', '6:4:1:1:2:4'}) end)
-hs.hotkey.bind(meh, "s", function () yabai({'-m', 'window', '--toggle', 'sticky'}) end)
+hs.hotkey.bind(meh, "space", function() yabai({ '-m', 'window', '--toggle', 'float', '--grid', '6:4:1:1:2:4' }) end)
+hs.hotkey.bind(meh, "s", function() yabai({ '-m', 'window', '--toggle', 'sticky' }) end)
 
-hs.hotkey.bind(meh, "b", function () yabai({'-m', 'space', '--balance'}) end)
-hs.hotkey.bind(meh, "r", function () yabai({'-m', 'space', '--rotate', '270'}) end)
-hs.hotkey.bind(meh, "x", function () yabai({'-m', 'space', '--mirror', 'x-axis'}); yabai({'-m', 'space', '--balance'}) end)
-hs.hotkey.bind(meh, "y", function () yabai({'-m', 'space', '--mirror', 'y-axis'}); yabai({'-m', 'space', '--balance'}) end)
+hs.hotkey.bind(meh, "b", function() yabai({ '-m', 'space', '--balance' }) end)
+hs.hotkey.bind(meh, "r", function() yabai({ '-m', 'space', '--rotate', '270' }) end)
+hs.hotkey.bind(meh, "x",
+    function()
+        yabai({ '-m', 'space', '--mirror', 'x-axis' }); yabai({ '-m', 'space', '--balance' })
+    end)
+hs.hotkey.bind(meh, "y",
+    function()
+        yabai({ '-m', 'space', '--mirror', 'y-axis' }); yabai({ '-m', 'space', '--balance' })
+    end)
 
-hs.hotkey.bind(meh, "n", function () yabai({'-m', 'window', '--space', 'next'}) end)
-hs.hotkey.bind(meh, "p", function () yabai({'-m', 'window', '--space', 'prev'}) end)
+hs.hotkey.bind(meh, "n", moveWindowToNextScreen)
+hs.hotkey.bind(meh, "p", moveWindowToPreviousScreen)
 
 -- shrink
 -- hs.hotkey.bind(meh, "e", function () yabai({'-m', 'window', '--resize', 'top:0:200'}) end)
