8e766971ae3796f96b79a7aed0f592dc09e24c76
Tomasz Walotek <tomasz.walotek@gmail.com>
2023-08-25 10:43:02 +0200

    nvim(zk): Update mappings

--- neovim/lua/mappings/zk.lua
+++ neovim/lua/mappings/zk.lua
@@ -2,12 +2,12 @@ return {
     name = '+zk',
     c = {
         function() vim.ui.input({ prompt = "Title: " }, function(i) vim.cmd('ZkNew {title="' .. i .. '"}') end) end,
-        'New note' },
+        'Create new note' },
     b = { '<Cmd>ZkBacklinks<CR>', 'Backlinks' },
     f = { '<Cmd>ZkNotes<CR>', 'Find a note' },
     i = { '<Cmd>ZkInsertLink<CR>', 'Insert link' },
     l = { '<Cmd>ZkLinks<CR>', 'Links' },
-    t = { '<Cmd>ZkNewFromTitleSelection<CR>', mode = 'v', 'New note from title selection' },
-    T = { '<Cmd>ZkTags<CR>', 'Tags' },
+    s = { '<Cmd>ZkNewFromTitleSelection<CR>', mode = 'v', 'New note from title selection' },
+    t = { '<Cmd>ZkTags<CR>', 'Tags' },
     v = { '<Cmd>ZkNewFromContentSelection<CR>', mode = 'v', 'New note from content selection' },
 }
