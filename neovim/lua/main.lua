5e09473779a76492a933b58efbe9527bd292e642
Tomasz Walotek <tomasz.walotek@gmail.com>
2023-08-25 09:45:23 +0200

    nvim: Rename plugins/lazy.lua to plugins/init.lua

--- neovim/lua/main.lua
+++ neovim/lua/main.lua
@@ -1,27 +1,27 @@
-require('plugins.lazy')
+require("plugins")
 
-require('impatient')
+require("impatient")
 vim.loader.enable()
 
-require('config.lsp')
-require('config.diagnostics')
+require("config.lsp")
+require("config.diagnostics")
 
 --
 -- custom highlights
 --
-vim.cmd [[highlight MatchArea guibg=#303030]]
-vim.cmd [[hi! ColorColumn guifg=#adbac7 guibg=#2d3239]]
+vim.cmd([[highlight MatchArea guibg=#303030]])
+vim.cmd([[hi! ColorColumn guifg=#adbac7 guibg=#2d3239]])
 
 --
 -- custom commands
 --
-vim.cmd [[command DiffOrig vert new | set bt=nofile | r ++edit # | 0d_ | diffthis | wincmd p | diffthis]]
+vim.cmd([[command DiffOrig vert new | set bt=nofile | r ++edit # | 0d_ | diffthis | wincmd p | diffthis]])
 -- reverse lines order
-vim.cmd [[command -range Reverse <line1>,<line2>!tac]]
+vim.cmd([[command -range Reverse <line1>,<line2>!tac]])
 
 --
 -- custom abbrevs
 --
-vim.cmd [[iabbrev <expr> YMD strftime("%Y-%m-%d")]]
+vim.cmd([[iabbrev <expr> YMD strftime("%Y-%m-%d")]])
 
-require('mappings')
+require("mappings")
