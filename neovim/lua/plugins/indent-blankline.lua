8983bf6e3f0365d7e466f4134c9b0503739d37eb
Tomasz Walotek <tomasz.walotek@gmail.com>
2023-10-10 15:35:58 +0200

    neovim: Update indent-blankline config

--- neovim/lua/plugins/indent-blankline.lua
+++ neovim/lua/plugins/indent-blankline.lua
@@ -1,21 +1,24 @@
+-- FIXME: https://github.com/lukas-reineke/indent-blankline.nvim/wiki/Migrate-to-version-3
+
 vim.cmd [[highlight IndentBlanklineIndent1 guifg=#444444 gui=nocombine]]
 -- -- vim.cmd [[highlight IndentBlanklineContextChar guifg=#666666 gui=nocombine]]
 vim.cmd [[highlight IndentBlanklineContextChar guifg=#909090 gui=nocombine]]
 -- vim.cmd [[highlight IndentBlanklineContextStart guisp=#909090 gui=underline]]
 
 local opts = {
-    use_treesitter = true,
-    space_char_blankline = " ",
-    -- char_list = {'│', '¦'},
-    char = '│',
-    show_first_indent_level = true,
-    show_current_context = true,
-    show_current_context_start = false,
-    char_highlight_list = {
-        "IndentBlanklineIndent1",
+    indent = {
+        -- char = '│',
+        char = '▏',
     },
-    buftype_exclude = { "terminal", "nofile" },
-    -- filetype_exclude = {},
+    scope = {
+        enabled = true,
+        show_start = true,
+    },
+    -- char_highlight_list = { "IndentBlanklineIndent1" },
+    exclude = {
+        filetypes = {},
+        buftypes = { "terminal", "nofile" },
+    },
 }
 
 return { opts = opts }
--- neovim/lua/plugins/init.lua
+++ neovim/lua/plugins/init.lua
@@ -159,8 +159,7 @@ require("lazy").setup({
     {
         "lukas-reineke/indent-blankline.nvim",
         main = "ibl",
-        opts = require(
-            "plugins.indent-blankline").opts
+        opts = require("plugins.indent-blankline").opts
     },
     {
         "gen740/SmoothCursor.nvim",
