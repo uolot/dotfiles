acf2db4c38e62575fabe80d4caf0259944450c80
Tomasz Walotek <tomasz.walotek@gmail.com>
2023-10-15 16:50:58 +0200

    neovim: Add lazy cmds for markdown preview

--- neovim/lua/plugins/init.lua
+++ neovim/lua/plugins/init.lua
@@ -299,6 +299,7 @@ require("lazy").setup({
     {
         "iamcco/markdown-preview.nvim",
         ft = 'markdown',
+        cmd = { "MarkdownPreviewToggle", "MarkdownPreview", "MarkdownPreviewStop" },
         build = function()
             vim.fn["mkdp#util#install"]()
         end,
