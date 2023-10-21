04f36a2b789b47dc999133f20d8812122118352f
Tomasz Walotek <tomasz.walotek@gmail.com>
2023-10-21 12:15:10 +0200

    neovim: Map current buffer fuzzy find to <leader>/

--- neovim/lua/mappings/telescope.lua
+++ neovim/lua/mappings/telescope.lua
@@ -13,5 +13,5 @@ return {
     L = { extensions.live_grep_args.live_grep_args, 'Live grep args' },
     r = { builtin.resume, 'Resume' },
     u = { extensions.undo.undo, 'Undo tree' },
-    z = { builtin.current_buffer_fuzzy_find, 'Current buffer fuzzy find' },
+    ['/'] = { builtin.current_buffer_fuzzy_find, 'Current buffer fuzzy find' },
 }
