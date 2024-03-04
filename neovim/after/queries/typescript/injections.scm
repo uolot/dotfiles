fd8db7ad21ddddfdb6dc2744ca1f65657eea0860
Tomasz Walotek <tomasz.walotek@gmail.com>
2024-03-04 15:18:56 +0100

    neovim: Highlight SQL in `this.addSql()` calls

---
+++ neovim/after/queries/typescript/injections.scm
@@ -0,0 +1,13 @@
+; extends
+
+(
+ (call_expression
+   function: (member_expression
+               property: (property_identifier) @_name (#eq? @_name "addSql"))
+   arguments:
+   (arguments
+     (string) @injection.content)
+   (#offset! @injection.content 0 1 0 -1)
+   (#set! injection.include-children)
+   (#set! injection.language "sql"))
+ )
