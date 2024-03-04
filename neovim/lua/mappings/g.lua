a490a1608adc20f54e26dde40e25949cd3583906
Tomasz Walotek <tomasz.walotek@gmail.com>
2024-03-04 15:12:47 +0100

    neovim: Fix switch buffer mappings

--- neovim/lua/mappings/g.lua
+++ neovim/lua/mappings/g.lua
@@ -1,6 +1,6 @@
 return {
-    B = { '<Cmd>bp', 'Previous buffer', silent = true },
-    b = { '<Cmd>bn', 'Next buffer', silent = true },
+    B = { '<Cmd>bp<CR>', 'Previous buffer', silent = true },
+    b = { '<Cmd>bn<CR>', 'Next buffer', silent = true },
     O = { ':<C-u>put! =repeat(nr2char(10), v:count1)<CR>', 'Insert blank lines above' },
     o = { ':<C-u>put =repeat(nr2char(10), v:count1)<CR>', 'Insert blank lines below' },
     V = { '`[v`]', 'Select inserted/pasted text', mode = 'x' },
