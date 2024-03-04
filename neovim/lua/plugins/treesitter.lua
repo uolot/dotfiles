380710fc0ed0d81690e10dac0cef82ddfd20329c
Tomasz Walotek <tomasz.walotek@gmail.com>
2024-03-04 15:13:30 +0100

    neovim: Remove old tresitter config

--- neovim/lua/plugins/treesitter.lua
+++ neovim/lua/plugins/treesitter.lua
@@ -179,24 +179,6 @@ local function config()
                 node_decremental = '<S-TAB>',
             },
         },
-        playground = {
-            enable = true,
-            disable = {},
-            updatetime = 25,         -- Debounced time for highlighting nodes in the playground from source code
-            persist_queries = false, -- Whether the query persists across vim sessions
-            keybindings = {
-                toggle_query_editor = 'o',
-                toggle_hl_groups = 'i',
-                toggle_injected_languages = 't',
-                toggle_anonymous_nodes = 'a',
-                toggle_language_display = 'I',
-                focus_language = 'f',
-                unfocus_language = 'F',
-                update = 'R',
-                goto_node = '<cr>',
-                show_help = '?',
-            },
-        }
     })
 
     vim.g.skip_ts_context_commentstring_module = true
