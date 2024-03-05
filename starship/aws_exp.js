1c2bc5f9867331a86f35e4a612a02d234a2fef4c
Tomasz Walotek <tomasz.walotek@gmail.com>
2024-03-05 12:27:44 +0100

    bash: starship: Add EPX and refactor AWS prompt

--- starship/aws_exp.js
+++ starship/aws_exp.js
@@ -1,84 +1,42 @@
 const fs = require("fs");
 
-let expires = "";
-const awsCredentialExpiration = process.env.AWS_CREDENTIAL_EXPIRATION;
+function expires() {
+  const awsCredentialExpiration = process.env.AWS_CREDENTIAL_EXPIRATION;
 
-try {
-  if (awsCredentialExpiration) {
-    const exp = new Date(awsCredentialExpiration);
-    const now = new Date();
-    const diffMs = exp - now;
-    const diffMinutes = Math.trunc(diffMs / 1000 / 60);
-    expires = diffMinutes > 0 ? `${diffMinutes}m` : "EXPIRED";
-  }
-} catch (e) {}
-
-let account = "";
-const profile = process.env.SAML2AWS_PROFILE;
-
-if (profile === "saml") {
   try {
-    const awsCredentialsContents = fs.readFileSync(
-      `${process.env.HOME}/.aws/credentials`,
-      { encoding: "utf8", flag: "r" }
-    );
-
-    if (awsCredentialsContents) {
-      const accounts = new Map([
-        ["409621694634", "LAC PROD"],
-        ["686442161031", "LAC QA"],
-        ["231207746339", "LAC DEV"],
-        ["976547032187", "CD PROD"],
-        ["878774257274", "CD DEV"],
-        ["064455468047", "CD SANDBOX"],
-      ]);
-
-      const lines = awsCredentialsContents
-        .split("\n")
-        .filter((line) => line.includes("x_principal_arn"));
-      const accountId = lines[0]
-        .split("=")
-        .pop()
-        .split(":")
-        .filter((line) => line.match(/\d+/))[0];
-
-      if (accounts.has(accountId)) {
-        account = accounts.get(accountId);
-      }
+    if (awsCredentialExpiration) {
+      const exp = new Date(awsCredentialExpiration);
+      const now = new Date();
+      const diffMs = exp - now;
+      const diffMinutes = Math.trunc(diffMs / 1000 / 60);
+      return diffMinutes > 0 ? `${diffMinutes}m` : "EXPIRED";
     }
   } catch (e) {}
-} else {
-  try {
-    const [accountId, role] = profile.split(":");
-
-    let accountName;
-
-    switch (accountId) {
-      case "lego-mct-ccd-prod":
-        accountName = "CD PROD";
-        break;
-      case "lego-mct-ccd-dev":
-        accountName = "CD DEV";
-        break;
-      case "lego-mct-ccd-sandbox":
-        accountName = "CD SANDBOX";
-        break;
-      case "lego-lac-prod":
-        accountName = "LAC PROD";
-        break;
-      case "lego-lac-qa":
-        accountName = "LAC QA";
-        break;
-      case "lego-lac-dev":
-        accountName = "LAC DEV";
-        break;
-      default:
-        accountName = "UNKNOWN (profile)";
-        break;
-    }
-
-    account = `${accountName} ${role}`;
-  } catch (e) {}
+  return "";
 }
 
-console.log(`${account} ${expires}`.trim());
+function accountName(accountId) {
+  const names = new Map([
+    ["lego-mct-ccd-prod", "CD PROD"],
+    ["lego-mct-ccd-dev", "CD DEV"],
+    ["lego-mct-ccd-sandbox", "CD SANDBOX"],
+    ["lego-lac-prod", "LAC PROD"],
+    ["lego-lac-qa", "LAC QA"],
+    ["lego-lac-dev", "LAC DEV"],
+    ["lego-epx-prod", "EPX PROD"],
+    ["lego-epx-qa", "EPX QA"],
+    ["lego-epx-dev", "EPX DEV"],
+    ["lego-epx-data-prod", "EPX DATA PROD"],
+  ]);
+
+  return names.get(accountId) ?? "UNKNOWN";
+}
+
+const profile = process.env.SAML2AWS_PROFILE;
+
+if (profile) {
+  const [accountId, role] = profile.split(":");
+  const name = accountName(accountId);
+
+  console.log(`${name} ${role} ${expires()}`.trim());
+}
