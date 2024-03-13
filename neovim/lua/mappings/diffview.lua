daebfe2c27a27eedead23e893cbba7e09eda7f99
Tomasz Walotek <tomasz.walotek@gmail.com>
2024-03-13 09:53:37 +0100

    neovim: Update diffview mappings

--- neovim/lua/mappings/diffview.lua
+++ neovim/lua/mappings/diffview.lua
@@ -2,5 +2,6 @@ return {
     name = '+diffview',
     c = { '<Cmd>DiffviewClose<CR>', 'Close' },
     f = { '<Cmd>DiffviewFileHistory %<CR>', 'File history' },
+    h = { '<Cmd>DiffviewFileHistory %<CR>', 'File history' },
     o = { '<Cmd>DiffviewOpen<CR>', 'Open' },
 }
