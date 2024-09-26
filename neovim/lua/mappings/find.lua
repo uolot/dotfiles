b4eb65f7ccffe80aec8ac86cd3853c728b5e5949
Tomasz Walotek <tomasz.walotek@gmail.com>
2024-09-26 10:51:27 +0200

    neovim: Update mini.files config

--- neovim/lua/mappings/find.lua
+++ neovim/lua/mappings/find.lua
@@ -1,11 +1,22 @@
 local telescope_builtin = require('telescope.builtin')
+local mini_files = require('mini.files')
 
 return {
     name = '+find',
+    c = { function()
+        local path = vim.bo.buftype ~= "nofile" and vim.api.nvim_buf_get_name(0) or nil
+        if not mini_files.close() then mini_files.open(path, false) end
+    end, 'Mini files - current file' },
     f = { telescope_builtin.find_files, 'Find files' },
     g = { telescope_builtin.git_status, 'Git status' },
     h = { '<Cmd>Telescope find_files hidden=true<CR>', 'Hidden files' },
-    m = { require('mini.files').open, 'Mini files' },
+    m = {
+        -- mini_files.open,
+        function()
+            if not mini_files.close() then mini_files.open() end
+        end,
+        'Mini files',
+    },
     o = { telescope_builtin.oldfiles, 'Open recent files' },
     p = {
         function()
