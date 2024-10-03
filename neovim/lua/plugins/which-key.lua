b9cc289b20606906595d985672c6ebe90ae10d08
Tomasz Walotek <tomasz.walotek@gmail.com>
2024-10-03 11:01:57 +0200

    neovim: Update which-key.lua config

--- neovim/lua/plugins/which-key.lua
+++ neovim/lua/plugins/which-key.lua
@@ -1,7 +1,12 @@
 local opts = {
-    -- spec = {},
-    preset = "helix",
+    preset = "modern",
     notify = true,
+    marks = true,
+    registers = true,
+    spelling = {
+        enabled = true,   -- enabling this will show WhichKey when pressing z= to select spelling suggestions
+        suggestions = 20, -- how many suggestions should be shown in the list?
+    },
     presets = {
         operators = true,
         motions = true,
