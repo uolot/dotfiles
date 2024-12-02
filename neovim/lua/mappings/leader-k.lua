9d3eb6547407b0b72a4d87d4b3bee1744987ce87
Tomasz Walotek <tomasz.walotek@gmail.com>
2024-12-02 12:39:36 +0100

    neovim: Fix marking words in visual mode

--- neovim/lua/mappings/leader-k.lua
+++ neovim/lua/mappings/leader-k.lua
@@ -1,11 +1,12 @@
 local wk = require("which-key")
 
 wk.add({
-    mode = { "n", "x" },
+    mode = { "n" },
     { "<Leader>k",  group = "Mark and highlight" },
     { "<Leader>kc", "<Plug>MarkClear",           desc = "Clear mark", },
     { "<Leader>kg", "V:<C-u>HSHighlight 5<CR>",  desc = "Highlight line green", },
     { "<Leader>kk", "<Plug>MarkSet",             desc = "Set mark", },
+    { "<Leader>kk", "<Plug>MarkSet",             desc = "Set mark",             mode = "x" },
     { "<Leader>kl", "V:<C-u>HSHighlight 1<CR>",  desc = "Highlight line", },
     { "<Leader>km", "<Plug>MarkToggle",          desc = "Toggle mark", },
     { "<Leader>kr", "V:<C-u>HSHighlight 4<CR>",  desc = "Highlight line red", },
