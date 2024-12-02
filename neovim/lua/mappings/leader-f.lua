f01190c0a5fec43bc1f4e4905320b87159c37f15
Tomasz Walotek <tomasz.walotek@gmail.com>
2024-12-02 12:39:11 +0100

    neovim: Fix mini.files mappings

--- neovim/lua/mappings/leader-f.lua
+++ neovim/lua/mappings/leader-f.lua
@@ -18,16 +18,21 @@ local function telescope_search_git_root()
     telescope_builtin.find_files({ cwd = git_root })
 end
 
+local function telescope_find_all()
+    telescope_builtin.find_files({ hidden = true, no_ignore = true })
+end
+
 wk.add({
     mode = "n",
     { "<Leader>f",  group = "+files" },
     -- Telescope
-    { "<Leader>ff", telescope_builtin.find_files,                desc = "Open files" },
-    { "<Leader>fg", telescope_builtin.git_status,                desc = "Open modified git files" },
-    { "<Leader>fh", "<Cmd>Telescope find_files hidden=true<CR>", desc = "Open hidden files" },
-    { "<Leader>fo", telescope_builtin.oldfiles,                  desc = "Open recent files" },
-    { "<Leader>fp", telescope_search_git_root,                   desc = "Open files in git root" },
+    { "<Leader>ff", telescope_builtin.find_files, desc = "Find files" },
+    { "<Leader>fg", telescope_builtin.git_status, desc = "Find modified git files" },
+    -- { "<Leader>fh", "<Cmd>Telescope find_files hidden=true no_ignore=true<CR>", desc = "Find hidden files" },
+    { "<Leader>fa", telescope_find_all,           desc = "Find all files" },
+    { "<Leader>fo", telescope_builtin.oldfiles,   desc = "Find recent files" },
+    { "<Leader>fp", telescope_search_git_root,    desc = "Find files in git root" },
     -- Mini files
-    { "<Leader>fc", mini_files_current,                          desc = "mini.files: Show current file" },
-    { "<Leader>f",  mini_files_toggle,                           desc = "mini.files: Open", },
+    { "<Leader>fc", mini_files_current,           desc = "mini.files: Show current file" },
+    { "<Leader>fm", mini_files_toggle,            desc = "mini.files: Open", },
 })
