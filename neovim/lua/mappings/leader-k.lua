a4f242ccc4a890c5e8d31dc9e24f0855a941ae83
Tomasz Walotek <tomasz.walotek@gmail.com>
2024-12-05 14:15:15 +0100

    neovim: Update vim-mark config

--- neovim/lua/mappings/leader-k.lua
+++ neovim/lua/mappings/leader-k.lua
@@ -1,14 +1,32 @@
 local wk = require("which-key")
 
 wk.add({
-    mode = { "n" },
-    { "<Leader>k",  group = "Mark and highlight" },
-    { "<Leader>kc", "<Plug>MarkClear",           desc = "Clear mark", },
-    { "<Leader>kg", "V:<C-u>HSHighlight 5<CR>",  desc = "Highlight line green", },
-    { "<Leader>kk", "<Plug>MarkSet",             desc = "Set mark", },
-    { "<Leader>kk", "<Plug>MarkSet",             desc = "Set mark",             mode = "x" },
-    { "<Leader>kl", "V:<C-u>HSHighlight 1<CR>",  desc = "Highlight line", },
-    { "<Leader>km", "<Plug>MarkToggle",          desc = "Toggle mark", },
-    { "<Leader>kr", "V:<C-u>HSHighlight 4<CR>",  desc = "Highlight line red", },
-    { "<Leader>kx", "V:<C-u>HSRmHighlight<CR>",  desc = "Remove highlight", },
+    { "<Leader>k", group = "Highlight" },
+    -- highlight words
+    {
+        mode = { "n" },
+        { "<Leader>kc", "<Plug>MarkClear",             desc = "Clear current mark" },
+        { "<Leader>kC", "<Plug>MarkAllClear",          desc = "Clear all marks" },
+        { "<Leader>kk", "<Plug>MarkSet",               desc = "Set mark" },
+        { "<Leader>km", "<Plug>MarkToggle",            desc = "Toggle mark" },
+        { "]k",         "<Plug>MarkSearchCurrentNext", desc = "Search current mark next" },
+        { "[k",         "<Plug>MarkSearchCurrentPrev", desc = "Search current mark prev" },
+        { "<Leader>kn", "<Plug>MarkSearchAnyNext",     desc = "Search any mark next" },
+        { "<Leader>kp", "<Plug>MarkSearchAnyPrev",     desc = "Search any mark prev" },
+        -- TODO: figure out how this works
+        -- { "<Leader>kw", "<Plug>MarkPartialWord",       desc = "Mark partial word" },
+        -- { "<Leader>ke", "<Plug>MarkRegex",             desc = "Mark regex" },
+        -- highlight lines
+        { "<Leader>kl", "V:<C-u>HSHighlight 1<CR>",    desc = "Highlight line" },
+        { "<Leader>kg", "V:<C-u>HSHighlight 5<CR>",    desc = "Highlight line green" },
+        { "<Leader>kr", "V:<C-u>HSHighlight 4<CR>",    desc = "Highlight line red" },
+        { "<Leader>kx", "V:<C-u>HSRmHighlight<CR>",    desc = "Remove line highlight" },
+    },
+    {
+        mode = { "x" },
+        { "<Leader>kk", "<Plug>MarkSet", desc = "Set mark" },
+        -- TODO: figure out how this works
+        -- { "<Leader>kw", "<Plug>MarkPartialWord", desc = "Mark partial word" },
+        -- { "<Leader>ke", "<Plug>MarkRegex",       desc = "Mark regex" },
+    },
 })
