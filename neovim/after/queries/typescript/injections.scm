c61865b23e18633f033ed8d2682158c5e4fdc041
Tomasz Walotek <tomasz.walotek@gmail.com>
2025-01-23 08:20:07 +0100

    neovim: Update TS injections for MikroORM queries in migration files

--- neovim/after/queries/typescript/injections.scm
+++ neovim/after/queries/typescript/injections.scm
@@ -11,3 +11,16 @@
    (#set! injection.include-children)
    (#set! injection.language "sql"))
  )
+
+
+(
+ (call_expression
+   function: (member_expression
+               property: (property_identifier) @_name (#eq? @_name "addSql"))
+   arguments:
+   (arguments
+     (template_string) @injection.content)
+   (#offset! @injection.content 0 1 0 -1)
+   (#set! injection.include-children)
+   (#set! injection.language "sql"))
+ )
