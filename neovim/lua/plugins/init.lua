159de2313f2343b8302da1d26c6ec773a8b489df
Tomasz Walotek <tomasz.walotek@gmail.com>
2025-01-24 10:17:48 +0100

    neovim: Remove yazi.nvim

--- neovim/lua/plugins/init.lua
+++ neovim/lua/plugins/init.lua
@@ -697,40 +697,6 @@ require("lazy").setup({
         end,
     },
 
-    ---@type LazySpec
-    {
-        "mikavilpas/yazi.nvim",
-        event = "VeryLazy",
-        keys = {
-            {
-                "<leader>yf",
-                "<cmd>Yazi<cr>",
-                desc = "Open yazi at the current file",
-            },
-            {
-                -- Open in the current working directory
-                "<leader>yy",
-                "<cmd>Yazi cwd<cr>",
-                desc = "Open the file manager in nvim's working directory",
-            },
-            {
-                -- NOTE: this requires a version of yazi that includes
-                -- https://github.com/sxyazi/yazi/pull/1305 from 2024-07-18
-                '<leader>yt',
-                "<cmd>Yazi toggle<cr>",
-                desc = "Resume the last yazi session",
-            },
-        },
-        ---@type YaziConfig
-        opts = {
-            -- if you want to open yazi instead of netrw, see below for more info
-            open_for_directories = false,
-            keymaps = {
-                show_help = 'g?',
-            },
-        },
-    },
-
     -- 99_end
 }, {
     ui = {
