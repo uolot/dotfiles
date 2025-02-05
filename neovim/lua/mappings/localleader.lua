d68d5a50549a2e9451ab64eb975cf6db30970dfc
Tomasz Walotek <tomasz.walotek@gmail.com>
2025-02-05 13:27:14 +0100

    neovim: Update localleader mappings for tabs

--- neovim/lua/mappings/localleader.lua
+++ neovim/lua/mappings/localleader.lua
@@ -9,7 +9,8 @@ end
 wk.add({
     mode = "n",
     { "<localleader>bd", delete_current_buffer, desc = "Delete current buffer" },
-    { "<localleader>t",  "<Cmd>tabnew<CR>",     desc = "Open new tab" },
+    { "<localleader>tn", "<Cmd>tabnew<CR>",     desc = "Open new tab" },
+    { "<localleader>tc", "<Cmd>tabclose<CR>",   desc = "Close current tab" },
     { "<localleader>n",  "<Cmd>enew<CR>",       desc = "Open new file" },
     { "<localleader>k",  "<Cmd>bnext<CR>",      desc = "Next buffer" },
     { "<localleader>j",  "<Cmd>bprev<CR>",      desc = "Previous buffer" },
