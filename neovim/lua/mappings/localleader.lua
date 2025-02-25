871136f0f8f9eff28f94bdf26468da3e973fcbfe
Tomasz Walotek <tomasz.walotek@gmail.com>
2025-02-25 16:20:15 +0100

    neovim: Add localleader mapping for pasting above from system clipboard

--- neovim/lua/mappings/localleader.lua
+++ neovim/lua/mappings/localleader.lua
@@ -1,5 +1,4 @@
 local wk = require("which-key")
-local wk_extras = require("which-key.extras")
 
 local function delete_current_buffer()
     local bufnr = vim.api.nvim_get_current_buf()
@@ -8,17 +7,34 @@ end
 
 wk.add({
     mode = "n",
-    { "<localleader>bd", delete_current_buffer, desc = "Delete current buffer" },
-    { "<localleader>tn", "<Cmd>tabnew<CR>",     desc = "Open new tab" },
-    { "<localleader>tc", "<Cmd>tabclose<CR>",   desc = "Close current tab" },
-    { "<localleader>n",  "<Cmd>enew<CR>",       desc = "Open new file" },
-    { "<localleader>k",  "<Cmd>bnext<CR>",      desc = "Next buffer" },
-    { "<localleader>j",  "<Cmd>bprev<CR>",      desc = "Previous buffer" },
-    { "<localleader>l",  "<Cmd>tabnext<CR>",    desc = "Next tab" },
-    { "<localleader>h",  "<Cmd>tabprev<CR>",    desc = "Previous tab" },
-    { "<localleader>qq", "<Cmd>cclose<CR>",     desc = "Close quickfix window" },
-    { "<localleader>qo", "<Cmd>copen<CR>",      desc = "Open quickfix window" },
-    { "<localleader>p",  '"+p',                 desc = "Paste from system clipboard" },
+    -- buffers
+    { "<localleader>n", "<Cmd>enew<CR>",    desc = "Open new buffer" },
+    {
+        { "<localleader>b",  group = "+bufs" },
+        { "<localleader>bn", "<Cmd>enew<CR>",       desc = "Open new buffer" },
+        { "<localleader>bd", delete_current_buffer, desc = "Delete current buffer" },
+    },
+    -- tabs
+    {
+        { "<localleader>t",  group = "+tabs" },
+        { "<localleader>tn", "<Cmd>tabnew<CR>",   desc = "Open new tab" },
+        { "<localleader>tc", "<Cmd>tabclose<CR>", desc = "Close current tab" },
+    },
+    -- buf nav
+    { "<localleader>k", "<Cmd>bnext<CR>",   desc = "Next buffer" },
+    { "<localleader>j", "<Cmd>bprev<CR>",   desc = "Previous buffer" },
+    -- tab nav
+    { "<localleader>l", "<Cmd>tabnext<CR>", desc = "Next tab" },
+    { "<localleader>h", "<Cmd>tabprev<CR>", desc = "Previous tab" },
+    -- quickfix
+    {
+        { "<localleader>q",  group = "+quickfix" },
+        { "<localleader>qq", "<Cmd>cclose<CR>",  desc = "Close quickfix window" },
+        { "<localleader>qo", "<Cmd>copen<CR>",   desc = "Open quickfix window" },
+    },
+    -- system clipboard
+    { "<localleader>p",     '"+p', desc = "Paste from system clipboard below" },
+    { "<localleader><S-p>", '"+P', desc = "Paste from system clipboard above" },
     {
         mode = { "n", "v" },
         { "<localleader>y", '"+y', desc = "Yank to system clipboard" },
