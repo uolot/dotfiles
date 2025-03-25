cfbda2a290b0c83fa08e18b2687f0ada55c7f520
Tomasz Walotek <tomasz.walotek@gmail.com>
2025-03-25 13:11:58 +0100

    neovim: Add lsp_status to lualine

--- neovim/lua/plugins/lualine.lua
+++ neovim/lua/plugins/lualine.lua
@@ -107,6 +107,20 @@ local opts = {
                     -- { 'title', 'percentage' },
                 },
             },
+            {
+                'lsp_status',
+                icon = '', -- f013
+                symbols = {
+                    -- Standard unicode symbols to cycle through for LSP progress:
+                    spinner = { '⠋', '⠙', '⠹', '⠸', '⠼', '⠴', '⠦', '⠧', '⠇', '⠏' },
+                    -- Standard unicode symbol for when LSP is done:
+                    done = '✓',
+                    -- Delimiter inserted between LSP names:
+                    separator = ' ',
+                },
+                -- List of LSP names to ignore (e.g., `null-ls`):
+                ignore_lsp = {},
+            },
         },
         lualine_y = {
             -- 'diff',
