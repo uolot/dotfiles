964102d89fdf7f7671b5620af81a0c148beeba9b
Tomasz Walotek <tomasz.walotek@gmail.com>
2025-03-27 09:38:12 +0100

    neovim: Map <leader>nh to notification history

--- neovim/lua/mappings/leader-n.lua
+++ neovim/lua/mappings/leader-n.lua
@@ -3,6 +3,7 @@ local wk = require("which-key")
 wk.add({
     mode = "n",
     { "<Leader>n",  group = "+explorer" },
-    { "<Leader>nc", Snacks.explorer.reveal, desc = "Reveal current file" },
-    { "<Leader>nn", Snacks.explorer.open,   desc = "Toggle" },
+    { "<Leader>nc", Snacks.explorer.reveal,       desc = "Reveal current file" },
+    { "<Leader>nn", Snacks.explorer.open,         desc = "Toggle" },
+    { "<Leader>nh", Snacks.notifier.show_history, desc = "Show notification history" },
 })
