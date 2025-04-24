c75e1ea926c533cbdf854d29d416136efb7798bf
Tomasz Walotek <tomasz.walotek@gmail.com>
2025-04-24 10:55:37 +0200

    neovim: Enable inlay hints for typescript

--- neovim/lua/config/lsp.lua
+++ neovim/lua/config/lsp.lua
@@ -154,6 +154,18 @@ local function config()
         volar = {},
         vtsls = {
             on_attach = on_tsserver_attach,
+            settings = {
+                typescript = {
+                    inlayHints = {
+                        parameterNames = { enabled = "all" },
+                        parameterTypes = { enabled = true },
+                        variableTypes = { enabled = true },
+                        propertyDeclarationTypes = { enabled = true },
+                        functionLikeReturnTypes = { enabled = true },
+                        enumMemberValues = { enabled = true },
+                    }
+                },
+            }
         },
         yamlls = {},
     }
