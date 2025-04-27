a6103cfd88e2ffae2f64a4d8c4ff45a077a8d7af
Tomasz Walotek <tomasz.walotek@gmail.com>
2025-04-27 17:07:05 +0000

    neovim: Add mappings for auto-closing brackets

--- neovim/lua/mappings/init.lua
+++ neovim/lua/mappings/init.lua
@@ -69,6 +69,18 @@ wk.add({
   },
 })
 
+wk.add({
+  mode = "i",
+  { "(<space>",    "()<left>" },
+  { "(<cr>",       "(<cr>)<esc>O" },
+  { "[<space>",    "[]<left>" },
+  { "[<cr>",       "[<cr>]<esc>O" },
+  { "{<space>",    "{}<left>" },
+  { "{<cr>",       "{<cr>}<esc>O" },
+  { "<lt><space>", "<lt>><left>" },
+  { "<lt><cr>",    "<lt><cr>><esc>O" },
+})
+
 require("mappings.g")
 require("mappings.z")
 require("mappings.c-w")
