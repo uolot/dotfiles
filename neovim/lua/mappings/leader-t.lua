716d53490b2479d8fa5408d53f3db8d4a14f2a58
Tomasz Walotek <tomasz.walotek@gmail.com>
2025-05-05 09:09:58 +0200

    neovim: Grep word in hidden files with <Leader>tG

--- neovim/lua/mappings/leader-t.lua
+++ neovim/lua/mappings/leader-t.lua
@@ -12,6 +12,18 @@ local function grep_word()
     })
 end
 
+local function grep_word_everywhere()
+    Snacks.picker.grep_word({
+        args = { "-w", "--hidden" },
+        matcher = {
+            ignorecase = false,
+        },
+        formatters = {
+            file = { truncate = 100 },
+        },
+    })
+end
+
 wk.add({
     mode = "n",
     { "<Leader>t",  group = "Telescope" },
@@ -20,6 +32,7 @@ wk.add({
     ---@diagnostic disable-next-line: undefined-field
     { "<Leader>td", Snacks.picker.todo_comments,   desc = 'Todos' },
     { "<Leader>tg", grep_word,                     desc = 'Grep word under cursor' },
+    { "<Leader>tG", grep_word_everywhere,          desc = 'Grep word under cursor' },
     { "<Leader>tl", Snacks.picker.grep,            desc = 'Live grep' },
     { "<Leader>tp", Snacks.picker.pickers,         desc = 'All pickers' },
     { "<Leader>tr", Snacks.picker.resume,          desc = 'Resume' },
