0b0be5b4ecdc3bcb66f923cebe27c4d1b6836302
Tomasz Walotek <tomasz.walotek@gmail.com>
2025-07-25 13:02:15 +0200

    neovim: Add text-case.nvim

--- neovim/lua/plugins/init.lua
+++ neovim/lua/plugins/init.lua
@@ -681,6 +681,27 @@ require("lazy").setup({
         end,
     },
 
+    {
+        "johmsalas/text-case.nvim",
+        config = function()
+            require("textcase").setup({
+                prefix = "sc",
+            })
+        end,
+        keys = {
+            "sc", -- Default invocation prefix
+        },
+        cmd = {
+            -- NOTE: The Subs command name can be customized via the option "substitude_command_name"
+            "Subs",
+            "TextCaseStartReplacingCommand",
+        },
+        -- If you want to use the interactive feature of the `Subs` command right away, text-case.nvim
+        -- has to be loaded on startup. Otherwise, the interactive feature of the `Subs` will only be
+        -- available after the first executing of it or after a keymap of text-case.nvim has been used.
+        lazy = false,
+    },
+
     -- 99_end
 }, {
     ui = {
