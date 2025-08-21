21b4da65220e154b6a84f700236a694e3943a388
Tomasz Walotek <tomasz.walotek@gmail.com>
2025-08-21 10:02:27 +0200

    neovim: Add kulala.nvim

--- neovim/lua/plugins/init.lua
+++ neovim/lua/plugins/init.lua
@@ -794,6 +794,28 @@ require("lazy").setup({
             },
         },
     },
+
+    {
+        "mistweaverco/kulala.nvim",
+        keys = {
+            { "<leader>Rs", desc = "Send request" },
+            { "<leader>Ra", desc = "Send all requests" },
+            { "<leader>Rb", desc = "Open scratchpad" },
+        },
+        ft = { "http", "rest" },
+        opts = {
+            global_keymaps = true,
+            global_keymaps_prefix = "<leader>R",
+            kulala_keymaps_prefix = "",
+            ui = {
+                formatter = true,
+            },
+            lsp = {
+                enable = true,
+                formatter = true,
+            },
+        },
+    },
     -- 99_end
 }, {
     ui = {
