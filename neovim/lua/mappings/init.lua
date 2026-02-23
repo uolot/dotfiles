afb4729450af25b0e817334ac0781ac919a014ca
Tomasz Walotek <tomasz.walotek@gmail.com>
2026-02-23 14:46:04 +0100

    neovim: Format mappings init.lua

--- neovim/lua/mappings/init.lua
+++ neovim/lua/mappings/init.lua
@@ -2,97 +2,109 @@ local wk = require("which-key")
 local snacks = require("snacks")
 
 local function close_floating()
-  for _, win in ipairs(vim.api.nvim_list_wins()) do
-    local config = vim.api.nvim_win_get_config(win)
-    if config.relative ~= "" and config.focusable then
-      vim.api.nvim_win_close(win, false)
-    end
-  end
+	for _, win in ipairs(vim.api.nvim_list_wins()) do
+		local config = vim.api.nvim_win_get_config(win)
+		if config.relative ~= "" and config.focusable then
+			vim.api.nvim_win_close(win, false)
+		end
+	end
 end
 
 local function next_usage()
-  snacks.words.jump(1, true)
+	snacks.words.jump(1, true)
 end
 
 local function previous_usage()
-  snacks.words.jump(-1, true)
+	snacks.words.jump(-1, true)
 end
 
 wk.add({
-  mode = "n",
-  { "<Esc>",       close_floating, desc = "Close all floating windows" },
-  { "<Backspace>", "<C-^>",        desc = "Quick buffer switch" },
-  { "<Tab>",       "<C-w>w",       desc = "Next window" },
-  { "<S-Tab>",     "<C-w>W",       desc = "Previous window" },
-  -- Unmap 's' and 'S' in normal mode
-  {
-    noremap = true,
-    silent = true,
-    { "s", "<Nop>" },
-    { "S", "<Nop>" },
-  },
-  -- Delete mark
-  { "dm",    ":delmark ",            desc = "Delete mark" },
-  -- Move between windows
-  {
-    { "<C-h>", "<C-w>h", desc = "Move left" },
-    { "<C-j>", "<C-w>j", desc = "Move down" },
-    { "<C-k>", "<C-w>k", desc = "Move up" },
-    { "<C-l>", "<C-w>l", desc = "Move right" },
-  },
-  -- Terminal
-  {
-    mode = "t",
-    { "<C-h>", "<C-\\><C-N><C-w>h", desc = "Move left" },
-    { "<C-j>", "<C-\\><C-N><C-w>j", desc = "Move down" },
-    { "<C-k>", "<C-\\><C-N><C-w>k", desc = "Move up" },
-    { "<C-l>", "<C-\\><C-N><C-w>l", desc = "Move right" },
-    { "<Esc>", "<C-\\><C-N>",       desc = " Exit terminal mode" },
-  },
-  -- Snacks terminal
-  { "<C-,>", Snacks.terminal.toggle, desc = "Toggle terminal" },
-  {
-    mode = "t",
-    { "<C-,>", Snacks.terminal.toggle, desc = "Exit terminal mode" },
-  },
-  -- Snacks scratch
-  { "<Leader>.", function() Snacks.scratch.open() end, desc = "Open scratch window" },
-  { "<Leader>/", Snacks.scratch.select,                desc = "Select scratch window" },
-  -- Jumps
-  {
-    noremap = true,
-    silent = true,
-    { -- search
-      { "\\c", "/\\c",                              desc = "Case insensitive search", silent = false },
-      { "\\s", "/\\<\\><left><left>",               desc = "Search for word",         silent = false },
-      { "n",   "(v:searchforward ? 'nzz' : 'Nzz')", expr = true },
-      { "N",   "(v:searchforward ? 'Nzz' : 'nzz')", expr = true },
-    },
-    { -- todo comments
-      { "[t", require("todo-comments").jump_prev, desc = "Previous todo comment" },
-      { "]t", require("todo-comments").jump_next, desc = "Next todo comment" },
-    },
-    { -- lsp next/prev
-      { "[n", previous_usage, desc = "Previous usage" },
-      { "]n", next_usage,     desc = "Next usage" },
-    },
-    { -- aerial outline symbols
-      { "[s", '<Cmd>AerialPrev<CR>', desc = "Previous symbol" },
-      { "]s", '<Cmd>AerialNext<CR>', desc = "Next symbol" },
-    },
-    { -- treesitter scope from snacks
-      { "[%", Snacks.scope.jump,                                   desc = "Scope top" },
-      { "]%", function() Snacks.scope.jump({ bottom = true }) end, desc = "Scope bottom" },
-    },
-  },
+	mode = "n",
+	{ "<Esc>", close_floating, desc = "Close all floating windows" },
+	{ "<Backspace>", "<C-^>", desc = "Quick buffer switch" },
+	{ "<Tab>", "<C-w>w", desc = "Next window" },
+	{ "<S-Tab>", "<C-w>W", desc = "Previous window" },
+	-- Unmap 's' and 'S' in normal mode
+	{
+		noremap = true,
+		silent = true,
+		{ "s", "<Nop>" },
+		{ "S", "<Nop>" },
+	},
+	-- Delete mark
+	{ "dm", ":delmark ", desc = "Delete mark" },
+	-- Move between windows
+	{
+		{ "<C-h>", "<C-w>h", desc = "Move left" },
+		{ "<C-j>", "<C-w>j", desc = "Move down" },
+		{ "<C-k>", "<C-w>k", desc = "Move up" },
+		{ "<C-l>", "<C-w>l", desc = "Move right" },
+	},
+	-- Terminal
+	{
+		mode = "t",
+		{ "<C-h>", "<C-\\><C-N><C-w>h", desc = "Move left" },
+		{ "<C-j>", "<C-\\><C-N><C-w>j", desc = "Move down" },
+		{ "<C-k>", "<C-\\><C-N><C-w>k", desc = "Move up" },
+		{ "<C-l>", "<C-\\><C-N><C-w>l", desc = "Move right" },
+		{ "<Esc>", "<C-\\><C-N>", desc = " Exit terminal mode" },
+	},
+	-- Snacks terminal
+	{ "<C-,>", Snacks.terminal.toggle, desc = "Toggle terminal" },
+	{
+		mode = "t",
+		{ "<C-,>", Snacks.terminal.toggle, desc = "Exit terminal mode" },
+	},
+	-- Snacks scratch
+	{
+		"<Leader>.",
+		function()
+			Snacks.scratch.open()
+		end,
+		desc = "Open scratch window",
+	},
+	{ "<Leader>/", Snacks.scratch.select, desc = "Select scratch window" },
+	-- Jumps
+	{
+		noremap = true,
+		silent = true,
+		{ -- search
+			{ "\\c", "/\\c", desc = "Case insensitive search", silent = false },
+			{ "\\s", "/\\<\\><left><left>", desc = "Search for word", silent = false },
+			{ "n", "(v:searchforward ? 'nzz' : 'Nzz')", expr = true },
+			{ "N", "(v:searchforward ? 'Nzz' : 'nzz')", expr = true },
+		},
+		{ -- todo comments
+			{ "[t", require("todo-comments").jump_prev, desc = "Previous todo comment" },
+			{ "]t", require("todo-comments").jump_next, desc = "Next todo comment" },
+		},
+		{ -- lsp next/prev
+			{ "[n", previous_usage, desc = "Previous usage" },
+			{ "]n", next_usage, desc = "Next usage" },
+		},
+		{ -- aerial outline symbols
+			{ "[s", "<Cmd>AerialPrev<CR>", desc = "Previous symbol" },
+			{ "]s", "<Cmd>AerialNext<CR>", desc = "Next symbol" },
+		},
+		{ -- treesitter scope from snacks
+			{ "[%", Snacks.scope.jump, desc = "Scope top" },
+			{
+				"]%",
+				function()
+					Snacks.scope.jump({ bottom = true })
+				end,
+				desc = "Scope bottom",
+			},
+		},
+	},
 })
 
 require("mappings.g")
 require("mappings.c-w")
-require('mappings.leader-a')
-require('mappings.leader-c')
-require('mappings.leader-f')
-require('mappings.leader-g')
+require("mappings.leader-a")
+require("mappings.leader-c")
+require("mappings.leader-f")
+require("mappings.leader-g")
 require("mappings.leader-h")
 require("mappings.leader-k")
 require("mappings.leader-l")
