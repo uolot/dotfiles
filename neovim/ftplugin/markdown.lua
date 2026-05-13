ecb6492b4e5cb81057ae9a0304069cbccb5affba
Tomasz Walotek <tomasz.walotek@gmail.com>
2026-05-13 11:04:55 +0200

    neovim: Add custom config for markdown via ftplugin

--- neovim/ftplugin/markdown.lua
+++ neovim/ftplugin/markdown.lua
@@ -1,1 +1,10 @@
 vim.opt_local.conceallevel = 0
+
+-- enable soft wrap
+vim.opt_local.wrap = true -- wrap long lines will wrap and continue on the next line
+vim.opt_local.linebreak = true -- break wrapped lines at a word boundary
+vim.opt_local.breakindent = true -- indent wrapped lines to visually align with the original line
+-- vim.opt_local.colorcolumn = "90"
+
+-- hard wrap at 90 characters
+-- vim.opt_local.textwidth = 90
